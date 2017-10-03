86955667ab58c8586abd1cf5737bdaa01759f2f8
Fernando Briones <32376843+fernandobriones@users.noreply.github.com>
2017-10-03 15:14:07 -0700

    script sounds

---
+++ Sounds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log(borrar esta prueba);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
